8b4c3a44316e1b606041278b369f43e0e40b1e2c
Emirkan Acar <nydaolian@gmail.com>
2019-10-03 21:49:57 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const {app, BrowserWindow, dialog} = require('electron');
+const ipc = require('electron').ipcMain;
+const path = require('path');
+const url = require('url');
+const os = require('os');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+      width: 400,
+      height: 200,
+      webPreferences: { nodeIntegration: true }
+  });
+
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'src/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  mainWindow.setMenu(null);
+  // development only
+  //mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', () => {
+    app.quit();
+  });
+
+  // file upload event render
+  ipc.on('open-file-dialog-for-file', (event => {
+      if(os.platform() == 'linux' || os.platform() == 'win32')
+      {
+          dialog.showOpenDialog({
+              properties: ['openFile']
+          }, (files) => {
+              if(files)
+              {
+                event.sender.send('selected-file', files[0]);
+              }
+          })
+      }else {
+          dialog.showOpenDialog({
+              properties: ['openFile', 'openDirectory']
+          }, (files) => {
+              if(files)
+              {
+                event.sender.send('selected-file', files[0]);
+              }
+          })
+      }
+  }));
+});
---
+++ src/script.js
@@ -0,0 +1,58 @@
+const ipc = require('electron').ipcRenderer;
+const remote = require('electron').remote;
+const shell = require('electron').shell;
+const detectFileType = require('detect-file-type');
+var fs = require('fs');
+var textract = require('textract');
+var path = require('path');
+var gtts = require('node-gtts')('tr');
+
+const uploadButton = document.getElementById('pdfUpload');
+
+uploadButton.addEventListener('click', (event) => {
+   ipc.send('open-file-dialog-for-file');
+});
+
+ipc.on('selected-file', (event, pathN) => {
+    console.log('path: ' + pathN);
+
+    let filePath = "" + pathN;
+    let fileName = path.basename(pathN, path.extname(pathN));
+
+    console.log(fileName);
+
+    detectFileType.fromFile(filePath, (err, res) => {
+        if(err) throw (err);
+
+        if(res.ext == 'pdf')
+        {
+            console.log("file is pdf");
+
+            textract.fromFileWithPath(filePath, function( error, text ) {
+                if(error) throw (error);
+
+                let userDataFile = remote.app.getPath('userData') + '/' + fileName + '.txt';
+                let convertedText = "" + text;
+
+                console.log(userDataFile);
+
+                fs.writeFile(userDataFile, convertedText, (error) => {
+                   if (error) throw (error);
+
+                   let convertedFile = remote.app.getPath('userData') + '/' + fileName + '.wav';
+
+                   gtts.save(convertedFile, convertedText, () => {
+                       console.log('save done');
+                       shell.openItem(convertedFile)
+                   })
+
+                });
+            })
+
+        }else {
+            console.log("file is not pdf")
+        }
+
+    });
+
+});
